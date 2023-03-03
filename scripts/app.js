0a0836f993fa682eaf4fe0dcdc49131cf68de641
Azamjon62 <azamjonx499@gmail.com>
2023-03-03 20:12:32 +0500

    2-savol

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+// let aSon = prompt("Biron bir son kiriting");
+
+// if (aSon % 15 == 0) {
+//   alert("fizzbuzz");
+// } else if (aSon % 3 == 0) {
+//   alert("fizz");
+// } else if (aSon % 5 == 0) {
+//   alert("buzz");
+// } else {
+//     alert("3 gaham 5 ga ham qoldiqsiz bo'linmaydi")
+// }
+
+
+
+
+
+
+// ------------------------------------------------------------  //
+
+
+
+
+
+// const points = [40, 100, 1];
+// points.sort(function (a, b) {
+//   return a - b;
+// });
+
+// console.log(points);
+
+
+
+
+
