a882dade9792200488b95a065102a2bd3665ca40
Azamjon62 <azamjonx499@gmail.com>
2023-03-03 20:31:06 +0500

    5-savol

--- scripts/app.js
+++ scripts/app.js
@@ -90,3 +90,57 @@
 
 
 // ------------------------------------------------------------  //
+
+
+
+
+
+
+
+let data = [
+  {
+    kurs_nomi: "HTML",
+    kurs_davomiyligi: 25,
+    kurs_narxi: 300000,
+  },
+  {
+    kurs_nomi: "CSS",
+    kurs_davomiyligi: 55,
+    kurs_narxi: 400000,
+  },
+  {
+    kurs_nomi: "JS",
+    kurs_davomiyligi: 70,
+    kurs_narxi: 500000,
+  },
+  {
+    kurs_nomi: "React",
+    kurs_davomiyligi: 40,
+    kurs_narxi: 500000,
+  },
+];
+
+
+for (let i in data) {
+    let a = data[0].kurs_davomiyligi;
+    let b = data[1].kurs_davomiyligi;
+    let c = data[2].kurs_davomiyligi;
+    let d = data[3].kurs_davomiyligi;
+    let jami = a + b + c + d;
+
+    let aa = data[0].kurs_narxi;
+    let bb = data[1].kurs_narxi;
+    let cc = data[2].kurs_narxi;
+    let dd = data[3].kurs_narxi;
+    let jamiNarx = aa + bb + cc + dd;
+
+    console.log(`Jami kirs davomiyligi ${jami} kun narxi ${jamiNarx} ming so'm`);
+
+}
+
+
+
+
+
+
+
